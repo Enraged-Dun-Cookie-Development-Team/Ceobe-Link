4cf13207ef9ce9ad3b4086c3a07b9a2f546ea743
Azide <rukuy@qq.com>
2024-04-19 17:38:39 +0800

    :bug: 调整打包BaseUrl，删除不需要图片

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,23 @@ import path from "path";
 
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+// import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vitejs.dev/config/
-export default defineConfig(async () => ({
-  plugins: [react()],
+export default defineConfig(async (mode) => ({
+  plugins: [
+    react(),
+    // viteSingleFile()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "@ui": path.resolve(__dirname, "./src/components/ui"),
     },
   },
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
+  base: mode.mode === "production" ? "./" : "/",
   server: {
     port: 1420,
     strictPort: true,
-    watch: {
-      // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
-    },
   },
 }));
